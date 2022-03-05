978295a284f6480392107b19410542863e7d0adf
Aishwarya Dev <aishd30.ad@gmail.com>
2022-03-05 00:47:33 -0800

    Added conditions and function extractor

---
+++ src/condition_extractor.py
@@ -0,0 +1,62 @@
+from fuzzer import *
+
+class FunctionAndBranchConditionsExtractor():
+    """Extract function for dataset generation and condition components"""
+
+    def __init__(self):
+        self.var_map = {}
+
+    def collectVariables(self, tree):
+        """Explores the AST and stores function assignment in a dictionary"""
+        def traverse(node):
+            if isinstance(node, ast.Assign):
+                self.var_map[node.targets[0].id] = node
+            for child in ast.iter_child_nodes(node):
+                traverse(child)
+        traverse(tree)
+        return self.var_map
+
+    def extractVariables(self, tree):
+        """Explores the AST and returns the function name used for variable assignment"""
+        variables = []
+
+        def traverse(node):
+            if isinstance(node, ast.Name):
+                if node.id in self.var_map:
+                    variables.append(astor.to_source(self.var_map[node.id].value).strip())
+            for child in ast.iter_child_nodes(node):
+                traverse(child)
+        traverse(tree)
+        return variables[0]
+
+    def collect_conditionComponents(self, tree):
+        """Explores the AST and extracts branch conditions and target function"""
+        conditionComponents = []
+        self.collectVariables(tree)
+        """Extract uncovered branches"""
+        branches_uncovered = symfz_ct.collect_uncovered_branches()
+        branches = [b for b, _ in branches_uncovered]
+
+        def traverse(node):
+            if isinstance(node, ast.If):
+                if node.lineno in branches:
+                    conditionComponentsDict = {}
+                    conditionComponentsDict["target_fn"] = self.extractVariables(node.test)
+                    conditionComponentsDict["branch_conditions"] = astor.to_source(node.test).strip()
+                    conditionComponents.append(conditionComponentsDict)
+            for child in ast.iter_child_nodes(node):
+                traverse(child)
+        traverse(tree)
+        return conditionComponents
+
+if __name__ == '__main__':
+
+    """Source AST generation for subject program fun"""
+    source_ast = ast.parse(inspect.getsource(fun))
+
+    funCondExtractor = FunctionAndBranchConditionsExtractor()
+
+    """Pass the function ast to extract branch conditions and target function"""
+    funCondExtractor.collect_conditionComponents(source_ast)
+
+    print(funCondExtractor.collect_conditionComponents(source_ast))
