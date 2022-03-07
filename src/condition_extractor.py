998ba0cc07d4f2a205d5aacb74ae3a4e06f091bc
Aishwarya Dev <aishd30.ad@gmail.com>
2022-03-06 18:14:14 -0800

    Updated to format output

--- src/condition_extractor.py
+++ src/condition_extractor.py
@@ -3,6 +3,7 @@ import astor
 import inspect
 
 from uncovered_branches_extractor import *
+from target_programs import functions_to_approximate
 
 class FunctionAndBranchConditionsExtractor():
     """Extract function for dataset generation and condition components"""
@@ -46,21 +47,66 @@ class FunctionAndBranchConditionsExtractor():
                 if node.lineno in branches:
                     conditionComponentsDict = {}
                     conditionComponentsDict["target_fn"] = self.extractVariables(node.test)
-                    conditionComponentsDict["branch_conditions"] = astor.to_source(node.test).strip()
+                    processed_branchConditions = self.process_branchConditions(astor.to_source(node.test).strip())
+                    conditionComponentsDict["branch_conditions"] = [{}]
+                    conditionComponentsDict["branch_conditions"][0]["operator"] = processed_branchConditions[0]
+                    conditionComponentsDict["branch_conditions"][0]["target"] = processed_branchConditions[1]
                     conditionComponents.append(conditionComponentsDict)
             for child in ast.iter_child_nodes(node):
                 traverse(child)
         traverse(tree)
         return conditionComponents
 
+    def process_branchConditions(self, branchCondition):
+        "Extract operand and target from branch conditions"
+        branchCondition = branchCondition.replace("(","").replace(")","")
+        branchConditionArray = branchCondition.split()
+        branchConditionArray.pop(0)
+        return branchConditionArray
+
+
 if __name__ == '__main__':
 
     """Source AST generation for subject program fun"""
-    source_ast = ast.parse(inspect.getsource(program_14))
+    source_ast = ast.parse(inspect.getsource(program_1))
 
     funCondExtractor = FunctionAndBranchConditionsExtractor(symfz_ct)
 
     """Pass the function ast to extract branch conditions and target function"""
     funCondExtractor.collect_conditionComponents(source_ast)
 
-    print(funCondExtractor.collect_conditionComponents(source_ast))
+    conditionComponents = funCondExtractor.collect_conditionComponents(source_ast)
+
+    "Process the condition components to obtain the function in memory and extract operands and target from branch conditions"
+    processed_conditionComponentsArray = []
+    processed_conditionComponentsDict = {}
+    processed_conditionComponentsDict["branch_conditions"] = []
+
+    target = conditionComponents[0]["target_fn"]
+    targetNew = ""
+
+    for char in target:
+        if char != "(":
+            targetNew += char
+        elif char == "(":
+            break 
+
+    processed_conditionComponentsDict["target_fn"] = locals()[targetNew]
+
+    for idx in range(len(conditionComponents)):
+        processed_conditionComponentsDict["branch_conditions"].append(conditionComponents[idx]["branch_conditions"][0])
+
+    processed_conditionComponentsArray.append(processed_conditionComponentsDict)
+
+    print("processed_conditionComponentsArray", processed_conditionComponentsArray)
+
+
+
+
+
+
+    
+
+
+
+    
