fd4f6a0f1a1f0ae92a0d1cdbb0ef34432e5d29bb
Fabrice Harel-Canada <fabriceyhc@gmail.com>
2022-03-08 19:59:50 -0800

    Update program_7.py

--- src/target_programs/program_7.py
+++ src/target_programs/program_7.py
@@ -4,10 +4,10 @@ def program_7(x: float):
     y:float = dl_textbook_fn(x)
     print(y)
 
+    if round(y, 0) == 100:
+        raise Exception("You found a hard-to-reach bug!")
+
     if y > 100:
         return "dl_textbook_fn returned a value more than 100!"
     elif y < 100:
         return "dl_textbook_fn returned a value less than 100!"
-
-    if round(y, 0) == 100:
-        raise Exception("You found a hard-to-reach bug!")
