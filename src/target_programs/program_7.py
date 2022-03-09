68d07bae44d7956cd0485a69ebd1e3ea7b952ec8
Fabrice Harel-Canada <fabriceyhc@gmail.com>
2022-03-08 22:09:05 -0800

    Update program_7.py

--- src/target_programs/program_7.py
+++ src/target_programs/program_7.py
@@ -2,13 +2,12 @@ from target_programs.functions_to_approximate import dl_textbook_fn
 
 def program_7(x: float):
     y:float = dl_textbook_fn(x)
-    print(y)
-
-    if y > 100:
-        print("dl_textbook_fn(x) returned a value more than 100!")
-    
-    if y < 100:
-        print("dl_textbook_fn returned a value less than 100!")
 
     if round(y, 0) == 100:
         raise Exception("You found a hard-to-reach bug!")
+
+    if y > 100:
+        print("dl_textbook_fn(x) > 100!", y)
+    
+    if y < 100:
+        print("dl_textbook_fn(x) < 100!", y)
